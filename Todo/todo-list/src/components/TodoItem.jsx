a5de5bf704072a76c29ae6968b0235e1224554b7
Alfiya <al.yausheva@gmail.com>
2025-06-04 23:41:26 +0300

    added some styles

--- Todo/todo-list/src/components/TodoItem.jsx
+++ Todo/todo-list/src/components/TodoItem.jsx
@@ -3,10 +3,12 @@ import { GrClose, GrEdit } from "react-icons/gr";
 
 const TodoItem = ({todo, completeTask, editTask, removeTask}) => {
   return <>
-  <div className='item-container'>
+  <div className='todo-container'>
     <div onClick={()=>completeTask(todo.id)} className={(todo.isCompleted) ? 'todo-item completed' : 'todo-item'}>{todo.task}</div>
-    <div onClick={()=>editTask(todo.id)}><GrEdit/></div>
-    <div onClick={()=>removeTask(todo.id)}><GrClose/></div>
+    <div className='todo-icon-container'>
+      <div onClick={()=>editTask(todo.id)}><GrEdit/></div>
+      <div onClick={()=>removeTask(todo.id)}><GrClose/></div>
+    </div>
   </div>
   
   </>
