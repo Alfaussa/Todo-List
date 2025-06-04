6023d078f81e301b151c152d916d69f53d7e95e5
Alfiya <al.yausheva@gmail.com>
2025-06-04 23:26:13 +0300

    qwe

--- Todo/todo-list/src/components/TodoItem.jsx
+++ Todo/todo-list/src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import { GrClose, GrEdit } from "react-icons/gr";
 
 const TodoItem = ({todo, completeTask, editTask, removeTask}) => {
   return <>
-  <div>
+  <div className='item-container'>
     <div onClick={()=>completeTask(todo.id)} className={(todo.isCompleted) ? 'todo-item completed' : 'todo-item'}>{todo.task}</div>
     <div onClick={()=>editTask(todo.id)}><GrEdit/></div>
     <div onClick={()=>removeTask(todo.id)}><GrClose/></div>
